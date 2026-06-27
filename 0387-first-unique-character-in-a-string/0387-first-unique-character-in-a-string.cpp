94303c1f5164a0d18d99ed494c95b2bb380d3b10
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-06-27 11:30:35 +0530

    Time: 17 ms (27.68%), Space: 14.9 MB (20.6%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -15,16 +15,23 @@ public:
 // }
 
 
-int firstUniqChar(string s) {
-    int freq[26] = {0};
-
-    for(char c : s)
-        freq[c - 'a']++;
-
-    for(int i = 0; i < s.size(); i++)
-        if(freq[s[i] - 'a'] == 1)
-            return i;
-
-    return -1;
-}
+    int firstUniqChar(string s){
+        int n=s.length();
+        unordered_map<char,int> m;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(m.find(s[i])!=m.end()){
+                m[s[i]]++;
+            }
+            else{
+                m[s[i]]=1;
+                q.push(i);
+            }        
+        }
+        while(!q.empty()){
+            if(m[s[q.front()]]>1){q.pop();}
+            else if(m[s[q.front()]]==1){return q.front();}
+        }
+        return -1;
+    }
 };
