97edc24f809201ae95d482c040e862288b861efc
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-06-30 12:13:42 +0530

    Time: 2 ms (30.08%), Space: 29 MB (70.25%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -17,17 +17,22 @@ public:
         bool right=chk(p->right,q->right);
         return left&&right&&p->val==q->val;
     }
-    bool fn(TreeNode* root, TreeNode* subRoot){
+    // bool fn(TreeNode* root, TreeNode* subRoot){
+    //     if(root==NULL){return false;}
+    //     if(root->val==subRoot->val&&chk(root,subRoot)){
+    //         return true;
+    //     }
+    //     bool left=fn(root->left,subRoot);
+    //     bool right=fn(root->right,subRoot);
+    //     return left||right;
+    // }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(root==NULL){return false;}
         if(root->val==subRoot->val&&chk(root,subRoot)){
             return true;
         }
-        bool left=fn(root->left,subRoot);
-        bool right=fn(root->right,subRoot);
+        bool left=isSubtree(root->left,subRoot);
+        bool right=isSubtree(root->right,subRoot);
         return left||right;
-    }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(fn(root,subRoot)){return true;}
-        return false;
     }
 };
