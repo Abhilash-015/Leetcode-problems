507e15e09371c5e9e6bf6a7d08d6cc30c7937758
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-07-12 07:27:51 +0530

    Time: 15 ms (99.47%), Space: 37.5 MB (97.88%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -3,18 +3,17 @@ public:
     vector<int> arrayRankTransform(vector<int>& a) {
         int n=a.size();
         vector<int> ans(n,0);
-        unordered_map<int,int> m;
-        vector<int> tp=a;
+        vector<pair<int,int>> tp(n);
+        for(int i=0;i<n;i++){
+            tp[i]={a[i],i};
+        }
+        if(tp.size()==0){return {};}
         sort(tp.begin(),tp.end());
         int rank=1;
-        for(int i=0;i<tp.size();i++){
-            if(m.find(tp[i])==m.end()){
-                m[tp[i]]=rank++;
-            }
-            else{}
-        }
-        for(int i=0;i<n;i++){
-            ans[i]=m[a[i]];
+        ans[tp[0].second]=1;
+        for(int i=1;i<n;i++){
+            if(tp[i].first!=tp[i-1].first){rank++;}
+            ans[tp[i].second]=rank;
         }
         return ans;
     }
