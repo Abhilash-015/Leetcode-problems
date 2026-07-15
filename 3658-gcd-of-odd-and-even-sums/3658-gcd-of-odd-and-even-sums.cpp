0b70b579db08b281ff53ac9ad64af9c57059048e
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-07-15 09:36:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-    while (b != 0) {
-        int rem = a % b;
-        a = b;
-        b = rem;
-    }
-    return a;
-}
     int gcdOfOddEvenSums(int n) {
-        int sodd=0,seven=0;
-        for(int i=1;i<=2*n;i++){
-            i%2==0?seven+=i:sodd+=i;
-        }
-        int ans=gcd(seven,sodd);
-        return ans;
+        return n;
     }
 };
