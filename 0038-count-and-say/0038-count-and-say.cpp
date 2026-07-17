72989eb865203d9ded9782727c0edf3c5ffa1efa
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-07-17 16:07:23 +0530

    Time: 55 ms (5.04%), Space: 125.3 MB (5.18%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string cnt(string s){
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            int cnt=0;
+            char piv=s[i];
+            while(piv==s[i]){
+                cnt++;i++;
+            }
+            i--;
+            ans=ans+ to_string(cnt)+piv;
+        }
+        return ans;
+    }
+    string countAndSay(int n) {
+        if(n==1){return "1";}
+        string ans="1";
+        for(int i=2;i<=n;i++){
+            ans=cnt(ans);
+        }
+        return ans;
+    }
+};
