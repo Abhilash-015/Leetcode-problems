83bff799bf236192d2c4fb3922d2c9fd094c7deb
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-07-25 13:53:00 +0530

    Time: 0 ms (100%), Space: 9.1 MB (17.86%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -3,16 +3,18 @@ public:
     int maxProduct(int n) {
         vector<int> v;
         int x=n;
+        if(n<10){return n;}
         while(x!=0){
             v.push_back(x%10);
             x/=10;
         }
-        int maxx=INT_MIN;
-        for(int i=0;i<v.size();i++){
-            for(int j=i+1;j<v.size();j++){
-                maxx=max(maxx,v[i]*v[j]);
-            }
-        }
-        return maxx;
+        // int maxx=INT_MIN;
+        // for(int i=0;i<v.size();i++){
+        //     for(int j=i+1;j<v.size();j++){
+        //         maxx=max(maxx,v[i]*v[j]);
+        //     }
+        // }
+        sort(v.begin(),v.end());
+        return v[v.size()-1]*v[v.size()-2];
     }
 };
