5eb8b43fb3ec1d1e3095008c7249c5ac172a3706
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-08-12 14:12:21 +0530

    Time: 54 ms (91.91%), Space: 149.1 MB (99.16%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -6,12 +6,13 @@ public:
         int ans=0;
         unordered_map<int,int> map;
         for(int i=0;i<n;i++){
-            if(map.find(arr[i])==map.end()){
-                map[arr[i]]=1;
-            }
-            else{
-                map[arr[i]]++;
-            }
+            // if(map.find(arr[i])==map.end()){
+            //     map[arr[i]]=1;
+            // }
+            // else{
+            //     map[arr[i]]++;
+            // }
+            ++map[arr[i]];
             while(map[arr[i]]>k){
                 map[arr[lo]]--;
                 lo++;
