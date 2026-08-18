48f66b1e2cb03bdd92bbf35089234b6404f55e73
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-08-18 15:59:51 +0530

    Time: 11 ms (7.01%), Space: 21 MB (6.65%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     
-    void hp(vector<string> & ans,string s,int idx,int n){
+    void hp(vector<string> & ans,string s,int idx,int n,int cnt){
+        if(cnt<0){return;}
         if(idx==n){
-            ans.push_back(s);
+           if(cnt==0){ ans.push_back(s);}
             return ;
         }
-        hp(ans,s+'(',idx+1,n);
-        hp(ans,s+')',idx+1,n);
+        hp(ans,s+'(',idx+1,n,cnt+1);
+        hp(ans,s+')',idx+1,n,cnt-1);
     }
-    bool check(string s){
-        int n=s.length();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
-            if(cnt<0){return false;}
-        }
-        if(cnt==0){return true;}
-        return false;
-    }
+    // bool check(string s){
+    //     int n=s.length();
+    //     int cnt=0;
+    //     for(int i=0;i<n;i++){
+    //         if(s[i]=='('){
+    //             cnt++;
+    //         }
+    //         else{
+    //             cnt--;
+    //         }
+    //         if(cnt<0){return false;}
+    //     }
+    //     if(cnt==0){return true;}
+    //     return false;
+    // }
     vector<string> generateParenthesis(int n) {
         string s="";
         int idx=1;
         vector<string>  ans;
-        hp(ans,s,0,2*n);
-        vector<string> fans;
-        for(int i=0;i<ans.size();i++){
-            if(check(ans[i])){fans.push_back(ans[i]);}
-        }
-        return fans;
+        hp(ans,s,0,2*n,0);
+        // vector<string> fans;
+        // for(int i=0;i<ans.size();i++){
+        //     if(check(ans[i])){fans.push_back(ans[i]);}
+        // }
+        return ans;
     }
 };
