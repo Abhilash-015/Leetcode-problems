ad470f5d920a33e0710bb1292a063b7b30b96218
Thaduri Abhilash <thaduriabhilash109@gmail.com>
2026-08-18 15:56:02 +0530

    Time: 74 ms (5.01%), Space: 74 MB (5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    void hp(vector<string> & ans,string s,int idx,int n){
+        if(idx==n){
+            ans.push_back(s);
+            return ;
+        }
+        hp(ans,s+'(',idx+1,n);
+        hp(ans,s+')',idx+1,n);
+    }
+    bool check(string s){
+        int n=s.length();
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+            if(cnt<0){return false;}
+        }
+        if(cnt==0){return true;}
+        return false;
+    }
+    vector<string> generateParenthesis(int n) {
+        string s="";
+        int idx=1;
+        vector<string>  ans;
+        hp(ans,s,0,2*n);
+        vector<string> fans;
+        for(int i=0;i<ans.size();i++){
+            if(check(ans[i])){fans.push_back(ans[i]);}
+        }
+        return fans;
+    }
+};
